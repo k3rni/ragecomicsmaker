4ba31f3e79a8d95acc9cbc16220ce8ebe05d5e7c
Krzysztof Zych <k33rni@gmail.com>
2020-11-24 20:46:46 +0100

    Improve image resize algorithm

--- src/main/java/pl/koziolekweb/ragecomicsmaker/gui/ImagePanel.java
+++ src/main/java/pl/koziolekweb/ragecomicsmaker/gui/ImagePanel.java
@@ -14,12 +14,9 @@ import pl.koziolekweb.ragecomicsmaker.model.Screen;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Collection;
 
 import static java.awt.image.BufferedImage.TYPE_4BYTE_ABGR;
 
@@ -44,8 +41,17 @@ public class ImagePanel extends JPanel {
 	private Screen selectedScreen;
 	private Image scaledInstance;
 
+	private Color frameColor = new Color(0x70_c8c8c8, true);
+
 	public ImagePanel() {
 		super();
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				fitImage();
+			}
+		});
+
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -96,46 +102,75 @@ public class ImagePanel extends JPanel {
 		super.paintComponent(graphics);
 		BufferedImage off = new BufferedImage(getWidth(), getHeight(), TYPE_4BYTE_ABGR);
 		Graphics buffer = off.getGraphics();
-		if (image != null) {
-			int targetWidth = getWidth();
-			int targetHeight = getWidth();
-			double proportion = countProportion(image);
-			if (proportion > 1.0) {
-				targetHeight *= 1 / proportion;
-			} else {
-				targetWidth *= 1 / proportion;
-			}
-			scaledInstance = image.getScaledInstance(targetWidth, targetHeight, Image.SCALE_SMOOTH);
-			buffer.drawImage(scaledInstance, 0, 0, null);
-			if (paintNewFrame) {
-				rdm.setColor(new Color(200, 200, 200, 200));
-				rdm.paintRectangle(buffer, startX, startY, currentX, currentY);
-			}
-		}
-		if (selectedScreen != null) {
-			Collection<Frame> frames = selectedScreen.getFrames();
-			if (!frames.isEmpty()) {
-				for (Frame frame : frames) {
-					if (frame.isVisible()) {
-						int scaledInstanceWidth = scaledInstance.getWidth(null);
-						int scaledInstanceHeight = scaledInstance.getHeight(null);
-						int sx = fsc.calculateSize(frame.getStartX(), scaledInstanceWidth);
-						int w = fsc.calculateSize(frame.getSizeX(), scaledInstanceWidth);
-						int sy = fsc.calculateSize(frame.getStartY(), scaledInstanceHeight);
-						int h = fsc.calculateSize(frame.getSizeY(), scaledInstanceHeight);
-
-						rdm.setColor(new Color(130, 130, 130, 100));
-						rdm.paintFrame(buffer, sx, sy, w, h);
-						rdm.paintFrameNumber(frame.getId() + "", buffer, sx, sy, w, h);
-					}
-				}
-			}
-		}
+		if (image != null) paintImage(buffer);
+		if (selectedScreen != null) paintFrames(buffer);
 		graphics.drawImage(off, 0, 0, null);
 		buffer.dispose();
 	}
 
-	private double countProportion(BufferedImage image) {
+	private void paintFrames(Graphics buffer) {
+		for (Frame frame : selectedScreen.getFrames()) {
+			if (!frame.isVisible()) continue;
+
+			int scaledInstanceWidth = scaledInstance.getWidth(null);
+			int scaledInstanceHeight = scaledInstance.getHeight(null);
+			int sx = fsc.calculateSize(frame.getStartX(), scaledInstanceWidth);
+			int w = fsc.calculateSize(frame.getSizeX(), scaledInstanceWidth);
+			int sy = fsc.calculateSize(frame.getStartY(), scaledInstanceHeight);
+			int h = fsc.calculateSize(frame.getSizeY(), scaledInstanceHeight);
+
+			rdm.setColor(new Color(130, 130, 130, 100));
+			rdm.paintFrame(buffer, sx, sy, w, h);
+			rdm.paintFrameNumber(frame.getId() + "", buffer, sx, sy, w, h);
+		}
+	}
+
+	private void paintImage(Graphics buf) {
+		if (scaledInstance == null)
+			fitImage();
+
+		buf.drawImage(scaledInstance, 0, 0, null);
+
+		if (paintNewFrame) {
+			rdm.setColor(frameColor);
+			rdm.paintRectangle(buf, startX, startY, currentX, currentY);
+		}
+	}
+
+	private void fitImage() {
+		if (image == null) return;
+
+		int imgHeight = Integer.MAX_VALUE;
+		int imgWidth = Integer.MAX_VALUE;
+		double aspectRatio = calculateAspectRatio(image);
+
+		if (aspectRatio > 1.0) {
+			// Image is wider than it is tall. Reduce height.
+			imgWidth = getWidth();
+			imgHeight = (int) (imgWidth / aspectRatio);
+
+			while (imgHeight > getHeight()) {
+				double ratio = (double) getHeight() / imgHeight;
+				imgWidth *= ratio;
+				imgHeight = (int) (imgWidth / aspectRatio);
+			}
+		} else {
+			// Image is taller than it is wide. Reduce width.
+			imgHeight = getHeight();
+			imgWidth = (int) (imgHeight * aspectRatio);
+
+			while (imgWidth > getWidth()) {
+				double ratio = (double) getWidth() / imgWidth;
+				imgHeight *= ratio;
+				imgWidth = (int) (imgHeight * aspectRatio);
+			}
+		}
+
+		scaledInstance = image.getScaledInstance(imgWidth, imgHeight,
+				Image.SCALE_SMOOTH);
+	}
+
+	private double calculateAspectRatio(BufferedImage image) {
 		return image.getWidth() / (double) image.getHeight();
 	}
 
