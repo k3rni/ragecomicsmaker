cc1717903b18b363c28a375921d51e0bd3220344
Krzysztof Zych <k33rni@gmail.com>
2020-12-17 23:55:45 +0100

    Cleanup

--- src/main/java/pl.koziolekweb.ragecomicsmaker/gui/RootController.java
+++ src/main/java/pl.koziolekweb.ragecomicsmaker/gui/RootController.java
@@ -16,7 +16,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Window;
 import org.controlsfx.control.Notifications;
 import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.GlyphFont;
